daf720c12139d623a42a2ca18f004b33821190af
jfisha96 <Josh@uws141691usr.uws.ualberta.ca>
2015-03-04 08:23:59 -0700

    Name

--- Script.js
+++ Script.js
@@ -3,7 +3,7 @@ document.getElementById("foot01").innerHTML =
 
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
-"<li><a href='Index.html'>Home</a></li>" +
+"<li><a href='index.html'>Home</a></li>" +
 "<li><a href='Banks.html'>Data</a></li>" +
 "<li><a href='About.html'>About</a></li>" +
 "</ul>";
