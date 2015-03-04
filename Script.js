dc21749b1d54756f6d9d5572eddec08256e3698e
jfisha96 <Josh@uws141691usr.uws.ualberta.ca>
2015-03-04 09:01:50 -0700

    Logos Fo Days!!!

--- Script.js
+++ Script.js
@@ -1,9 +1,9 @@
-document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + " W3Schools. All rights reserved.</p>";
-
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
 "<li><a href='index.html'>Home</a></li>" +
 "<li><a href='Banks.html'>Data</a></li>" +
 "<li><a href='About.html'>About</a></li>" +
 "</ul>";
+
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " W3Schools. All rights reserved.</p>";
