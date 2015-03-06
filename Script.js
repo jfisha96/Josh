dd4b9ca4ab4211f52e570298e9f1208ca37e6406
jfisha96 <Josh@Joshuas-MacBook-Pro-2.local>
2015-03-05 20:29:20 -0700

    Social Media Add

--- Script.js
+++ Script.js
@@ -4,6 +4,3 @@ document.getElementById("nav01").innerHTML =
 "<li><a href='Banks.html'>Data</a></li>" +
 "<li><a href='About.html'>About</a></li>" +
 "</ul>";
-
-document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + " W3Schools. All rights reserved.</p>";
